5d90e97d4f62e7415328fd7f118afba6fc67cd7d
Tom <hernandeztomas584@gmail.com>
2024-03-23 14:57:24 -0300

    feat: end cobertura

--- Taller 2/taller2/src/test/java/aed/CoberturaTests.java
+++ Taller 2/taller2/src/test/java/aed/CoberturaTests.java
@@ -11,16 +11,26 @@ class CoberturaTests {
 
     @Test
     void testFizzBuzz() {
-        assertEquals("A","B");
+        assertEquals(cobertura.fizzBuzz(15), "FizzBuzz");
+        assertEquals(cobertura.fizzBuzz(6), "Fizz");
+        assertEquals(cobertura.fizzBuzz(20), "Buzz");
+        assertEquals(cobertura.fizzBuzz(2), "2");
     }
 
     @Test
     void testNumeroCombinatorio() {
-        assertTrue(false);
+        assertTrue(cobertura.numeroCombinatorio(1, 0) == 1);
+        assertTrue(cobertura.numeroCombinatorio(2, 2) == 1);
+        assertTrue(cobertura.numeroCombinatorio(2, 3) == 0);
+        assertEquals(cobertura.numeroCombinatorio(4, 3), 4);
     }
 
     @Test
     void testRepeticionesConsecutivas() {
-        assertTrue(false);
+        assertTrue(cobertura.repeticionesConsecutivas(new int[0]) == 0);
+        assertTrue(cobertura.repeticionesConsecutivas(new int[]{}) == 0);
+        assertTrue(cobertura.repeticionesConsecutivas(new int[]{1, 2, 3, 4}) == 1);
+        assertTrue(cobertura.repeticionesConsecutivas(new int[]{1, 1, 3, 1}) == 2);
+        assertTrue(cobertura.repeticionesConsecutivas(new int[]{1, 1, 1, 1}) == 4);
     }
 }
