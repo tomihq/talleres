8215615f29af4e025ee3140a8dd4e1f1251206c0
Tom <hernandeztomas584@gmail.com>
2024-04-26 19:30:33 -0300

    taller 3

---
+++ Taller 3/taller3/src/main/java/aed/Fecha.java
@@ -0,0 +1,48 @@
+package aed;
+
+public class Fecha {
+    int dia;
+    int mes; 
+
+    public Fecha(int dia, int mes) {
+        this.dia = dia;
+        this.mes = mes; 
+    }
+
+    public Fecha(Fecha fecha) {
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public Integer dia() {
+        return this.dia;
+    }
+
+    public Integer mes() {
+        return this.mes; 
+    }
+
+    @Override
+    public String toString() {
+        return this.dia + "/" + this.mes;
+    }
+
+    @Override
+    public boolean equals(Object otraFecha) {
+        return otraFecha.getClass() == this.getClass();
+    }
+
+    public void incrementarDia() {
+        this.dia = this.diasEnMes(this.mes) < this.dia + 1 ? 1 : this.dia+1; 
+    }
+
+    private int diasEnMes(int mes) {
+        int dias[] = {
+                // ene, feb, mar, abr, may, jun
+                31, 28, 31, 30, 31, 30,
+                // jul, ago, sep, oct, nov, dic
+                31, 31, 30, 31, 30, 31
+        };
+        return dias[mes - 1];
+    }
+
+}
